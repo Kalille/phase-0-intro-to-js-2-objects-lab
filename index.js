ce7009fff8f7ae4108294c9f2b4e7d7cae3985d2
kalille Overton <>
2021-04-26 22:33:55 -0400

    commit tree 9cbf644bd3155a1836e19876e4b1b0edecd0143b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+// const updateEmployeeWithKeyAndValue={
+    const employee={
+        name: 'Kalille',
+    streetAddress: '4418 Market st',
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmp ={...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee[key] = value
+     return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmp ={...employee}
+    delete newEmp[key]
+    return newEmp
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
